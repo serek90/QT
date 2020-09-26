d436a35399c19d59efcb99da307ab2c9628396e2
serek90 <43785920+serek90@users.noreply.github.com>
2020-09-26 21:52:34 +0200

    Add first aplication to repository

--- Lesson1/main.cpp
+++ Lesson1/main.cpp
@@ -1,11 +1,12 @@
-#include "calculator.h"
+#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Calculator w;
+    MainWindow w;
     w.show();
+    w.setWindowTitle("First APP");
 
     return a.exec();
 }
--- QT_ArduinoUART/mainwindow.cpp
+++ QT_ArduinoUART/mainwindow.cpp
@@ -3,7 +3,8 @@
 
 #include <QtSerialPort/QSerialPortInfo>
 #include <QList>
-#include<QDebug>
+#include <QDebug>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,9 +22,21 @@ void MainWindow::on_pushButtonSzukaj_clicked()
 {
     QList<QSerialPortInfo> devices;
     devices = QSerialPortInfo::availablePorts();
+
+    //Loop to search serial port
     for(int i =0; i < devices.count(); i++)
     {
         qDebug() << devices.at(i).portName() << devices.at(i).description();
+
+        ui->comboBoxDevices->addItem(devices.at(i).portName() + " " + devices.at(i).description());
     }
     qDebug() << "Szukam urządzeń ...";
+
+    //print serial port name
+    int index = 0;
+    if((devices.count() - 1) >= index)
+    {
+        devices.at(index).portName();
+    }
 }
+
